1c8dabfabe9ca9fb53f4c250fa7668a6a67cd8fc
Majss00n <michal.majewski.lublin@gmail.com>
2023-09-16 20:21:13 +0200

    about me site font and text added

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -11,7 +11,7 @@ export default function About() {
         <link rel = "stylesheet" href = "styles/about.scss"/>
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com"/>
-        <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@600&display=swap" rel="stylesheet"/>
+        <link href="https://fonts.googleapis.com/css2?family=Cinzel+Decorative:wght@700&family=Dancing+Script:wght@600&family=Amatic+SC&display=swap" rel="stylesheet"/>
       </head>
 
       <body>
@@ -40,11 +40,18 @@ export default function About() {
             </div>
 
             <div id = "aboutMeText">
-              <h1>Specjalistka od naturalnych efektów</h1>
-              <p>Witaj na mojej stronie, jestem Julia Majewska, 
+              <h1 className = "text-center">Specjalistka od naturalnych efektów</h1>
+              <p className = "text-left ml-5 mr-5">Witaj na mojej stronie, jestem Julia Majewska, 
                 i cieszę się, że możesz poznać mnie i moją pasję do modelowania ust oraz zabiegów na twarz. 
                 Jako specjalista w dziedzinie estetyki twarzy, moim celem jest pomóc Ci osiągnąć naturalny i wyjątkowy wygląd, 
                 który podkreśli Twoje piękno w sposób subtelny i harmonijny.</p>
+            </div>
+
+            <div id = "passion">
+              <h1 className = "text-center">Pasja do estetyki twarzy</h1>
+              <p className = "text-center">Od dawna fascynuje mnie sztuka modelowania twarzy i podkreślanie indywidualnego piękna każdej osoby. 
+                Dzięki zaawansowanym technikom i najnowszym osiągnięciom w dziedzinie estetyki twarzy, 
+                mogę pomóc Ci osiągnąć wymarzony wygląd, jednocześnie dbając o zachowanie naturalności i równowagi.</p>
             </div>
           </div>
         </main>
