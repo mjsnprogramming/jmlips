e55fef228d5808f99b7c6584349ba06ab4b074ab
Majss00n <majsonlbn@gmail.com>
2024-07-27 17:13:20 +0200

    english files

---
+++ src/app/EN/components/HeaderComponent_en.tsx
@@ -0,0 +1,40 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import '../../styles/header.scss'
+import 'tailwindcss/tailwind.css'
+
+export default function HeaderComponent() {
+      
+    return (
+        <header>
+        
+        <div id = "headerMenu">
+            <div id = "headerEmail">
+              <span>juliet.majewska@gmail.com</span>
+            </div>
+            <div id = "headerPhoneNumber">
+              <span>+48 234 566 345</span>
+            </div>
+            <div id = "logo">
+              <Link href = "/">
+                <Image src = "/images/logo.jpg" alt = "logo" className = "logo" width = "100" height = "100"></Image>
+              </Link>
+          </div>
+          <ul className = "flex justify-center items-center">
+            <li><Link href = "/about">O MNIE</Link></li>
+            <li><Link href = "/offer">OFERTA</Link></li>
+            <li><Link href = "/priceList">CENNIK</Link></li>
+            <li><Link href = "/sale">PROMOCJA</Link></li>
+            <li><Link href = "/contact">KONTAKT</Link></li>
+          </ul>
+          <div id = "bookingButton">
+            <button id = "booking">UMÓW SIĘ</button>
+          </div>
+          <div id = "languageButton">
+            <button id = "polish">PL</button>
+            <button id = "english">EN</button>
+          </div>
+          </div>
+        </header>
+    )    
+}
